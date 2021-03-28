7a4f223a4f5bcd5ce38cc2a85e5f5b4e51ccc642
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-28 18:26:07 -0300

    Finalizando VelocMedia

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string D, T;
+            int Velo, Distancia, Tempo;
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("/ Calculador de Velocidade /");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("");
+
+            Console.Write("Distancia Percorrida (m): ");
+            D = Console.ReadLine();
+            Console.Write("Tempo Gasto (s): ");
+            T = Console.ReadLine();
+
+            Distancia = Convert.ToInt32(D);
+            Tempo = Convert.ToInt32(T);
+
+            Velo = (Distancia / Tempo);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Velocidade Média: {Velo} m/s ");
+        }
+    }
+}
